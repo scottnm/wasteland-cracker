3997ebff6d8d2ceacf25969541d8d2aea1cb4daa
Scott Munro <scottnmunro@gmail.com>
2021-01-23 14:03:26 -0800

    quick iterspike on form

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,6 @@
 // Work breakdown
 // - constrain the number of words generated to only as much as would fit in two panes
 // - setup a better word selection algorithm which results in more common letters
-// - setup the win-lose condition that you only have 4 guesses
 // - render two panes
 // - place the words throughout the pane w/ filler text that goes in between words
 // - add support for selecting between the words in the TUI and highlighting the current selection
@@ -9,6 +8,8 @@
 //      - keyboard support?
 // - add support for using that selection instead of text input to power the gameloop
 // - add an output pane which tells you the results of your current selection
+// - refactor out tui utils into its own module
+// - randomize the hex start address for fun flavor
 
 // extensions/flavor
 // - use appropriate font to give it a "fallout feel"
@@ -16,6 +17,7 @@
 
 use crate::dict;
 use crate::randwrapper::{select_rand, RangeRng, ThreadRangeRng};
+use crate::utils::Rect;
 use std::str::FromStr;
 
 const TITLE: &str = "FONV: Terminal Cracker";
@@ -27,6 +29,23 @@ pub enum Difficulty {
     Average,
     Hard,
     VeryHard,
+}
+
+struct HexDumpPane {
+    dump_width: i32,
+    dump_height: i32,
+    addr_width: i32,
+    addr_to_dump_padding: i32,
+}
+
+impl HexDumpPane {
+    fn height(&self) -> i32 {
+        self.dump_height
+    }
+
+    fn full_width(&self) -> i32 {
+        self.dump_width + self.addr_to_dump_padding + self.addr_width
+    }
 }
 
 impl FromStr for Difficulty {
@@ -57,7 +76,7 @@ impl FromStr for Difficulty {
     }
 }
 
-pub fn generate_words(difficulty: Difficulty, rng: &mut dyn RangeRng<usize>) -> Vec<String> {
+fn generate_words(difficulty: Difficulty, rng: &mut dyn RangeRng<usize>) -> Vec<String> {
     let word_len = match difficulty {
         Difficulty::VeryEasy => 4,
         Difficulty::Easy => 6,
@@ -72,6 +91,16 @@ pub fn generate_words(difficulty: Difficulty, rng: &mut dyn RangeRng<usize>) -> 
     (0..WORDS_TO_GENERATE_COUNT)
         .map(|_| dict_chunk.get_random_word(rng))
         .collect()
+}
+
+fn render_hexdump_pane(
+    window: &pancurses::Window,
+    hex_dump_dimensions: &HexDumpPane,
+    render_rect: &Rect,
+    mem_start: usize,
+    bytes: &[char],
+) {
+    unimplemented!();
 }
 
 pub fn run_game(difficulty: Difficulty) {
@@ -93,10 +122,72 @@ pub fn run_game(difficulty: Difficulty) {
         // TODO just open a stub window for now. We'll write the game soon.
         window.clear();
 
+        const HEXDUMP_ROW_WIDTH: i32 = 12; // 12 characters per row of the hexdump
+        const HEXDUMP_MAX_ROWS: i32 = 16; // 16 rows of hex dump per dump pane
+        const HEXDUMP_MAX_BYTES: i32 = HEXDUMP_ROW_WIDTH * HEXDUMP_MAX_ROWS;
+        const MEMADDR_HEX_WIDTH: i32 = "0x1234".len() as i32; // 2 byte memaddr
+        const PANE_HORIZONTAL_PADDING: i32 = 4; // horizontal padding between panes in the memdump window
+
+        const HEXDUMP_PANE_WIDTH: i32 = MEMADDR_HEX_WIDTH
+            + PANE_HORIZONTAL_PADDING
+            + HEXDUMP_ROW_WIDTH
+            + (PANE_HORIZONTAL_PADDING / 2);
+
+        const HEXDUMP_PANE_VERT_OFFSET: i32 = 5;
+
+        let (window_height, window_width) = window.get_max_yx();
+        let (window_center_x, window_center_y) = (window_width / 2, window_height / 2);
+
+        // are all of these constants only used by this struct?
+        let hex_dump_pane_dimensions = HexDumpPane {
+            dump_width: HEXDUMP_ROW_WIDTH,
+            dump_height: HEXDUMP_MAX_ROWS,
+            addr_width: MEMADDR_HEX_WIDTH,
+            addr_to_dump_padding: PANE_HORIZONTAL_PADDING,
+        };
+
+        let left_hex_dump_rect = Rect {
+            left: window_center_x
+                - hex_dump_pane_dimensions.full_width()
+                - (PANE_HORIZONTAL_PADDING / 2),
+            top: HEXDUMP_PANE_VERT_OFFSET,
+            width: hex_dump_pane_dimensions.full_width(),
+            height: hex_dump_pane_dimensions.height(),
+        };
+
+        let right_hex_dump_rect = Rect {
+            left: left_hex_dump_rect.left
+                + left_hex_dump_rect.width
+                + (PANE_HORIZONTAL_PADDING / 2),
+            top: left_hex_dump_rect.top,
+            width: hex_dump_pane_dimensions.full_width(),
+            height: hex_dump_pane_dimensions.height(),
+        };
+
+        let hexdump_temp_fill = vec!['x'; HEXDUMP_MAX_BYTES as usize];
+        let hexdump_first_addr = 0x1234; // TODO: randomize for fun flavor
+        render_hexdump_pane(
+            &window,
+            &hex_dump_pane_dimensions,
+            &left_hex_dump_rect,
+            hexdump_first_addr,
+            &hexdump_temp_fill,
+        );
+
+        render_hexdump_pane(
+            &window,
+            &hex_dump_pane_dimensions,
+            &right_hex_dump_rect,
+            hexdump_first_addr + hexdump_temp_fill.len(),
+            &hexdump_temp_fill,
+        );
+
+        /* TODO: render the words in the memdump
         window.mvaddstr(0, 0, format!("{:?}", difficulty));
         for (i, rand_word) in rand_words.iter().enumerate() {
             window.mvaddstr(i as i32 + 1, 0, rand_word);
         }
+        */
 
         window.refresh();
         std::thread::sleep(std::time::Duration::from_millis(3000));
--- src/utils.rs
+++ src/utils.rs
@@ -8,3 +8,30 @@ pub fn matching_char_count_ignore_case(a: &str, b: &str) -> usize {
         .filter(chars_eq_ignore_case)
         .count()
 }
+
+pub struct Rect {
+    pub left: i32,
+    pub top: i32,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl Rect {
+    pub const fn _right(&self) -> i32 {
+        let right = self.left + self.width - 1;
+        right
+    }
+
+    pub const fn _bottom(&self) -> i32 {
+        let bottom = self.top + self.height - 1;
+        bottom
+    }
+
+    pub const fn _center_x(&self) -> i32 {
+        self.left + self.width / 2
+    }
+
+    pub const fn _center_y(&self) -> i32 {
+        self.top + self.height / 2
+    }
+}
