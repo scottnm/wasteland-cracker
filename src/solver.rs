b5e5ddc8861d10cafae2e6f24572e3394fd4ff5e
Scott Munro <scottnmunro@gmail.com>
2021-02-07 23:17:22 -0800

    highlight entry whose input to update

--- src/solver.rs
+++ src/solver.rs
@@ -117,15 +117,14 @@ pub fn solver(password_file: &str, guess_args: &[String], window: &pancurses::Wi
 
     let mut menu_cursor: i32 = 0;
     let cursor_prefix = "> ";
+    let cursor_prefix_len = cursor_prefix.len() as i32;
     let word_column_width = remaining_passwords.iter().map(|p| p.len()).max().unwrap() as i32;
     let padding_width = 4;
     let char_count_column_width = 2; // 00
 
     let menu_rect = {
-        let menu_width = (cursor_prefix.len() as i32)
-            + word_column_width
-            + padding_width
-            + char_count_column_width;
+        let menu_width =
+            cursor_prefix_len + word_column_width + padding_width + char_count_column_width;
         let menu_height = remaining_passwords.len() as i32;
 
         Rect {
@@ -157,7 +156,7 @@ pub fn solver(password_file: &str, guess_args: &[String], window: &pancurses::Wi
 
         for (i, pwd) in remaining_passwords.iter().enumerate() {
             let row = i as i32 + menu_rect.top;
-            let col_offset = menu_rect.left + cursor_prefix.len() as i32;
+            let col_offset = menu_rect.left + cursor_prefix_len;
             window.mvaddstr(row, col_offset, pwd);
             window.mvaddstr(
                 row,
@@ -166,21 +165,29 @@ pub fn solver(password_file: &str, guess_args: &[String], window: &pancurses::Wi
             );
         }
 
-        let back_button_row = menu_rect.top + (remaining_passwords.len() + 2) as i32;
+        let back_button_row = menu_rect.top + (remaining_passwords.len() + 1) as i32;
         let back_button_text = "[ Back ]";
         window.mvaddstr(
             back_button_row,
-            menu_rect.left + cursor_prefix.len() as i32,
+            menu_rect.left + cursor_prefix_len,
             back_button_text,
         );
 
         if (menu_cursor as usize) < remaining_passwords.len() {
-            window.mvaddstr(menu_rect.top + menu_cursor, menu_rect.left, cursor_prefix);
+            let cursor_row = menu_rect.top + menu_cursor;
+            window.mvaddstr(cursor_row, menu_rect.left, cursor_prefix);
+            window.mvchgat(
+                cursor_row,
+                menu_rect.left + cursor_prefix_len + menu_rect.width - char_count_column_width,
+                char_count_column_width,
+                pancurses::A_BLINK,
+                0,
+            );
         } else {
             assert_eq!(menu_cursor as usize, remaining_passwords.len());
             window.mvchgat(
                 back_button_row,
-                menu_rect.left + cursor_prefix.len() as i32,
+                menu_rect.left + cursor_prefix_len,
                 back_button_text.len() as i32,
                 pancurses::A_BLINK,
                 0,
