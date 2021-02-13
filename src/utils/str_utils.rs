8310a9a2d7cf4b87b19f2437e098f2901a1705b5
Scott Munro <scottnmunro@gmail.com>
2021-02-13 14:13:32 -0800

    added strutils test

--- src/utils/str_utils.rs
+++ src/utils/str_utils.rs
@@ -15,3 +15,22 @@ pub fn hamming_dist_ignore_case(a: &str, b: &str) -> usize {
     assert_eq!(a.len(), b.len());
     a.len() - matching_char_count_ignore_case(a, b)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matching_char_count_ignore_case() {
+        assert_eq!(matching_char_count_ignore_case("apple", "APpLe"), 5);
+        assert_eq!(matching_char_count_ignore_case("apple", "loodo"), 0);
+        assert_eq!(matching_char_count_ignore_case("upper", "APpLe"), 2);
+    }
+
+    #[test]
+    fn test_hamming_distance() {
+        assert_eq!(hamming_dist_ignore_case("apple", "APpLe"), 0);
+        assert_eq!(hamming_dist_ignore_case("apple", "loodo"), 5);
+        assert_eq!(hamming_dist_ignore_case("upper", "APpLe"), 3);
+    }
+}
